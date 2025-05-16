62e609aeab482e983ad54945f9b0f8d139e88d73
athashn <sabrina.atha@ui.ac.id>
2025-05-16 17:44:54 +0700

    edit final dop

---
+++ src-winvmj/LibraryManagementSystem.user.core/LibraryManagementSystem/user/core/model/User.java
@@ -0,0 +1,8 @@
+package LibraryManagementSystem.user.core;
+import vmj.routing.route.Route;
+import vmj.routing.route.VMJExchange;
+import java.util.*;
+
+public interface User {
+	HashMap<String, Object> toHashMap();
+}
---
+++ src-winvmj/LibraryManagementSystem.user.core/LibraryManagementSystem/user/core/resource/UserResourceDecorator.java
@@ -0,0 +1,34 @@
+package LibraryManagementSystem.user.core;
+import java.util.*;
+
+import vmj.routing.route.Route;
+import vmj.routing.route.VMJExchange;
+
+public abstract class UserResourceDecorator extends UserResourceComponent{
+	protected UserResourceComponent record;
+
+    public UserResourceDecorator(UserResourceComponent record) {
+        this.record = record;
+    }
+
+    public User createUser(VMJExchange vmjExchange){
+		return record.createUser(vmjExchange);
+	}
+
+    public HashMap<String, Object> updateUser(VMJExchange vmjExchange){
+		return record.updateUser(vmjExchange);
+	}
+
+    public HashMap<String, Object> getUser(VMJExchange vmjExchange){
+		return record.getUser(vmjExchange);
+	}
+
+    public List<HashMap<String,Object>> getAllUser(VMJExchange vmjExchange){
+		return record.getAllUser(vmjExchange);
+	}
+
+    public List<HashMap<String,Object>> deleteUser(VMJExchange vmjExchange){
+		return record.deleteUser(vmjExchange);
+	}
+
+}
